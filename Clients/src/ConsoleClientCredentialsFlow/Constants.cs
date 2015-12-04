07112582e6059fc74db5c118f982d5277ccbd52c
Dominick Baier <dbaier@leastprivilege.com>
2015-12-04 09:59:27 +0100

    Added console client cred flow

---
+++ Clients/src/ConsoleClientCredentialsFlow/Constants.cs
@@ -0,0 +1,16 @@
+﻿namespace Clients
+{
+    public class Constants
+    {
+        public const string BaseAddress = "http://localhost:22530";
+
+        public const string AuthorizeEndpoint = BaseAddress + "/connect/authorize";
+        public const string LogoutEndpoint = BaseAddress + "/connect/endsession";
+        public const string TokenEndpoint = BaseAddress + "/token";
+        public const string UserInfoEndpoint = BaseAddress + "/connect/userinfo";
+        public const string IdentityTokenValidationEndpoint = BaseAddress + "/connect/identitytokenvalidation";
+        public const string TokenRevocationEndpoint = BaseAddress + "/connect/revocation";
+
+        public const string AspNetWebApiSampleApi = "http://localhost:2727/";
+    }
+}
