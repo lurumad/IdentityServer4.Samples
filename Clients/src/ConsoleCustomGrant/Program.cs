ab9e268dbeaa312e6a1c82b320aa9d86aba4d7b1
Dominick Baier <dbaier@leastprivilege.com>
2015-12-22 12:11:48 +0100

    added custom grant client

---
+++ Clients/src/ConsoleCustomGrant/Program.cs
@@ -0,0 +1,91 @@
+﻿using Clients;
+using IdentityModel;
+using IdentityModel.Client;
+using IdentityModel.Extensions;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace ConsoleCustomGrant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var response = RequestToken();
+            ShowResponse(response);
+
+            Console.ReadLine();
+            CallService(response.AccessToken);
+        }
+
+        static TokenResponse RequestToken()
+        {
+            var client = new TokenClient(
+                Constants.TokenEndpoint,
+                "client.custom",
+                "secret");
+
+            var customParameters = new Dictionary<string, string>
+                {
+                    { "custom_credential", "custom credential"}
+                };
+
+            return client.RequestCustomGrantAsync("custom", "api1", customParameters).Result;
+        }
+
+        static void CallService(string token)
+        {
+            var baseAddress = Constants.AspNetWebApiSampleApi;
+
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(baseAddress)
+            };
+
+            client.SetBearerToken(token);
+            var response = client.GetStringAsync("identity").Result;
+
+            "\n\nService claims:".ConsoleGreen();
+            Console.WriteLine(JArray.Parse(response));
+        }
+
+        private static void ShowResponse(TokenResponse response)
+        {
+            if (!response.IsError)
+            {
+                "Token response:".ConsoleGreen();
+                Console.WriteLine(response.Json);
+
+                if (response.AccessToken.Contains("."))
+                {
+                    "\nAccess Token (decoded):".ConsoleGreen();
+
+                    var parts = response.AccessToken.Split('.');
+                    var header = parts[0];
+                    var claims = parts[1];
+
+                    Console.WriteLine(JObject.Parse(Encoding.UTF8.GetString(Base64Url.Decode(header))));
+                    Console.WriteLine(JObject.Parse(Encoding.UTF8.GetString(Base64Url.Decode(claims))));
+                }
+            }
+            else
+            {
+                if (response.IsHttpError)
+                {
+                    "HTTP error: ".ConsoleGreen();
+                    Console.WriteLine(response.HttpErrorStatusCode);
+                    "HTTP error reason: ".ConsoleGreen();
+                    Console.WriteLine(response.HttpErrorReason);
+                }
+                else
+                {
+                    "Protocol error response:".ConsoleGreen();
+                    Console.WriteLine(response.Json);
+                }
+            }
+        }
+    }
+}
