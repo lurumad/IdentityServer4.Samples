a2af77f40ef81c1e35e6a558fb7b1af89421d328
Dominick Baier <dbaier@leastprivilege.com>
2016-01-04 20:17:00 +0100

    updated to validation MW beta1

--- Clients/SampleApi/Startup.cs
+++ Clients/SampleApi/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNet.Builder;
+﻿using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -37,27 +38,16 @@ namespace SampleApi
 
             app.UseIISPlatformHandler();
 
-            // can use one or the other right now due to bug in Jwt MW RC1
-            //app.UseJwtBearerAuthentication(options =>
-            //{
-            //    options.AutomaticAuthenticate = true;
-            //    options.AutomaticChallenge = true;
-            //    options.Authority = Clients.Constants.BaseAddress;
-            //    options.TokenValidationParameters.ValidateAudience = false;
-            //    options.RequireHttpsMetadata = false;
-            //});
-
-            app.UseOAuth2IntrospectionAuthentication(options =>
+            app.UseIdentityServerAuthentication(options =>
             {
+                options.Authority = Clients.Constants.BaseAddress;
+                options.ScopeName = "api1";
+                options.ScopeSecret = "secret";
+
                 options.AutomaticAuthenticate = true;
                 options.AutomaticChallenge = true;
 
-                options.ScopeName = "api1";
-                options.ScopeSecret = "secret";
-                options.Authority = Clients.Constants.BaseAddress;
-
-                // use introspection endpoint for both JWTs and reference tokens (see above)
-                options.SkipTokensWithDots = false;
+                options.SaveTokenAsClaim = true;
             });
 
             //app.AllowScopes("calendar.read", "calendar.readwrite");
