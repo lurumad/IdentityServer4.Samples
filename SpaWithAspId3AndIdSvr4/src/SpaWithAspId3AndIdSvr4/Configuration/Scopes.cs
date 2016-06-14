cbb9a14974ba0fb4861a3ccdc2e1bf278b6d8c1e
Brock Allen <brockallen@gmail.com>
2016-06-14 16:30:06 -0400

    First stab at SPA + AspId3 sample

---
+++ SpaWithAspId3AndIdSvr4/src/SpaWithAspId3AndIdSvr4/Configuration/Scopes.cs
@@ -0,0 +1,31 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaWithAspId3AndIdSvr4.Configuration
+{
+    public class Scopes
+    {
+        public static IEnumerable<Scope> Get()
+        {
+            return new List<Scope>
+            {
+                StandardScopes.OpenId,
+                StandardScopes.Profile,
+                new Scope
+                {
+                    Name = "api.todo",
+                    DisplayName = "TODO API",
+                    Description = "TODO features and data",
+                    Type = ScopeType.Resource,
+                    Claims = new List<ScopeClaim>
+                    {
+                        new ScopeClaim("role")
+                    }
+                },
+            };
+        }
+    }
+}
---
+++ SpaWithAspId3AndIdSvr4/src/SpaWithAspId3AndIdSvr4/Startup.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SpaWithAspId3AndIdSvr4.Data;
+using SpaWithAspId3AndIdSvr4.Models;
+using SpaWithAspId3AndIdSvr4.Services;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using SpaWithAspId3AndIdSvr4.Configuration;
+using IdentityServer4.Services;
+using IdentityModel;
+using Microsoft.AspNetCore.Identity;
+
+namespace SpaWithAspId3AndIdSvr4
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets();
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+            Environment = env;
+        }
+
+        public IConfigurationRoot Configuration { get; }
+        public IHostingEnvironment Environment { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var cert = new X509Certificate2(Path.Combine(Environment.ContentRootPath, "idsrvtest.pfx"), "idsrv3test");
+            var builder = services.AddIdentityServer(options=>
+            {
+                options.AuthenticationOptions = new IdentityServer4.Configuration.AuthenticationOptions
+                {
+                    PrimaryAuthenticationScheme = "Cookies"
+                };
+            })
+            .AddInMemoryClients(Clients.Get())
+            .AddInMemoryScopes(Scopes.Get())
+            .SetSigningCredentials(cert);
+
+            services.AddTransient<IProfileService, AspIdProfileService>();
+
+            // Add framework services.
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(options=>
+            {
+                options.Cookies.ApplicationCookie.AuthenticationScheme = "Cookies";
+                options.ClaimsIdentity.UserIdClaimType = JwtClaimTypes.Subject;
+                options.ClaimsIdentity.UserNameClaimType = JwtClaimTypes.Name;
+                options.ClaimsIdentity.RoleClaimType = JwtClaimTypes.Role;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+            services.AddTransient<IUserClaimsPrincipalFactory<ApplicationUser>, IdentityServerUserClaimsPrincipalFactory>();
+
+            services.AddMvc();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, AuthMessageSender>();
+            services.AddTransient<ISmsSender, AuthMessageSender>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseIdentity();
+
+            // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
+
+            app.UseIdentityServer();
+
+            app.UseJwtBearerAuthentication(new JwtBearerOptions {
+                Authority = "http://localhost:1861",
+                Audience = "http://localhost:1861/resources",
+                AutomaticAuthenticate = true,
+                AuthenticationScheme = "Bearer",
+                RequireHttpsMetadata = false
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
