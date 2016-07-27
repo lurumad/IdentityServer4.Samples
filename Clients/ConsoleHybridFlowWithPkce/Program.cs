0104cb24385b283192cef6663878c36599f3a96c
Dominick Baier <dbaier@leastprivilege.com>
2016-07-27 11:57:48 +0200

    added console hybrid + pkce client sample

---
+++ Clients/ConsoleHybridFlowWithPkce/Program.cs
@@ -0,0 +1,143 @@
+﻿using IdentityModel.OidcClient;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHybridFlowWithPkce
+{
+    class Program
+    {
+        const string authority = "http://localhost:1941";
+
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                            .MinimumLevel.Debug()
+                            .WriteTo.LiterateConsole()
+                            .CreateLogger();
+
+
+            Console.WriteLine("+-----------------------+");
+            Console.WriteLine("|  Sign in with OIDC    |");
+            Console.WriteLine("+-----------------------+");
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to sign in...");
+            Console.ReadKey();
+
+            Program p = new Program();
+            p.SignIn();
+
+            Console.ReadKey();
+        }
+
+        private async void SignIn()
+        {
+            // create a redirect URI using an available port on the loopback address.
+            string redirectUri = string.Format("http://127.0.0.1:7890/");
+            Console.WriteLine("redirect URI: " + redirectUri);
+
+            // create an HttpListener to listen for requests on that redirect URI.
+            var http = new HttpListener();
+            http.Prefixes.Add(redirectUri);
+            Console.WriteLine("Listening..");
+            http.Start();
+
+            var options = new OidcClientOptions(
+                authority,
+                "console.hybrid.pkce",
+                "not_used",
+                "openid profile api1",
+                redirectUri);
+
+            options.UseFormPost = true;
+            options.Style = OidcClientOptions.AuthenticationStyle.Hybrid;
+
+            var client = new OidcClient(options);
+            var state = await client.PrepareLoginAsync();
+
+            Console.WriteLine($"Start URL: {state.StartUrl}");
+
+            // open system browser to start authentication
+            Process.Start(state.StartUrl);
+
+            // wait for the authorization response.
+            var context = await http.GetContextAsync();
+
+            var formData = GetRequestPostData(context.Request);
+
+            // Brings the Console to Focus.
+            BringConsoleToFront();
+
+            // sends an HTTP response to the browser.
+            var response = context.Response;
+            string responseString = $"<html><head><meta http-equiv='refresh' content='10;url={authority}'></head><body>Please return to the app.</body></html>";
+            var buffer = Encoding.UTF8.GetBytes(responseString);
+            response.ContentLength64 = buffer.Length;
+            var responseOutput = response.OutputStream;
+            await responseOutput.WriteAsync(buffer, 0, buffer.Length);
+            responseOutput.Close();
+
+            Console.WriteLine($"Form Data: {formData}");
+            var result = await client.ValidateResponseAsync(formData, state);
+
+            if (result.Success)
+            {
+                Console.WriteLine("\n\n\n\nClaims:");
+                foreach (var claim in result.Claims)
+                {
+                    Console.WriteLine("{0}: {1}", claim.Type, claim.Value);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Access token:\n{0}", result.AccessToken);
+
+                if (!string.IsNullOrWhiteSpace(result.RefreshToken))
+                {
+                    Console.WriteLine("Refresh token:\n{0}", result.RefreshToken);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n\nError:\n{0}", result.Error);
+            }
+
+            http.Stop();
+        }
+
+        private void BringConsoleToFront()
+        {
+            SetForegroundWindow(GetConsoleWindow());
+        }
+
+        private string GetRequestPostData(HttpListenerRequest request)
+        {
+            if (!request.HasEntityBody)
+            {
+                return null;
+            }
+
+            using (var body = request.InputStream)
+            {
+                using (var reader = new System.IO.StreamReader(body, request.ContentEncoding))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        // Hack to bring the Console window to front.
+        // ref: http://stackoverflow.com/a/12066376
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        public static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+    }
+}
