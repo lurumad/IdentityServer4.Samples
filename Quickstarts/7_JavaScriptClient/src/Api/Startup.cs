705bc7d53ea8ce2bf894bd8fd8c7c4adbd3fa1bf
Brock Allen <brockallen@gmail.com>
2016-09-05 18:14:46 -0400

    small updates to JS QS

--- Quickstarts/7_JavaScriptClient/src/Api/Startup.cs
+++ Quickstarts/7_JavaScriptClient/src/Api/Startup.cs
@@ -28,6 +28,7 @@ namespace Api
         {
             services.AddCors(options=>
             {
+                // this defines a CORS policy called "default"
                 options.AddPolicy("default", policy =>
                 {
                     policy.WithOrigins("http://localhost:5003")
@@ -46,6 +47,7 @@ namespace Api
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
+            // this uses the policy called "default"
             app.UseCors("default");
 
             app.UseIdentityServerAuthentication(new IdentityServerAuthenticationOptions
