49e7d073203983da30a860b1628e8b514856dc19
Dominick Baier <dbaier@leastprivilege.com>
2016-09-28 19:11:42 +0200

    added JWT client assertion sample

---
+++ Clients/src/ConsolePrivateKeyJwtClient/Program.cs
@@ -0,0 +1,119 @@
+﻿using Clients;
+using IdentityModel;
+using IdentityModel.Client;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using Newtonsoft.Json.Linq;
+using System.Text;
+
+namespace ConsolePrivateKeyJwtClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var response = RequestToken();
+            ShowResponse(response);
+
+            Console.ReadLine();
+            CallService(response.AccessToken);
+        }
+
+        static TokenResponse RequestToken()
+        {
+            var clientToken = CreateClientToken("client.jwt", Constants.TokenEndpoint);
+            var client = new TokenClient(Constants.TokenEndpoint, "client.jwt");
+
+            var assertion = new
+            {
+                client_assertion_type = OidcConstants.ClientAssertionTypes.JwtBearer,
+                client_assertion = clientToken
+            };
+
+            return client.RequestClientCredentialsAsync("api1", assertion).Result;
+        }
+
+        static void CallService(string token)
+        {
+            var baseAddress = Constants.AspNetWebApiSampleApi;
+
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(baseAddress)
+            };
+
+            client.SetBearerToken(token);
+            var response = client.GetStringAsync("identity").Result;
+
+            "\n\nService claims:".ConsoleGreen();
+            Console.WriteLine(JArray.Parse(response));
+        }
+
+        private static void ShowResponse(TokenResponse response)
+        {
+            if (!response.IsError)
+            {
+                "Token response:".ConsoleGreen();
+                Console.WriteLine(response.Json);
+
+                if (response.AccessToken.Contains("."))
+                {
+                    "\nAccess Token (decoded):".ConsoleGreen();
+
+                    var parts = response.AccessToken.Split('.');
+                    var header = parts[0];
+                    var claims = parts[1];
+
+                    Console.WriteLine(JObject.Parse(Encoding.UTF8.GetString(Base64Url.Decode(header))));
+                    Console.WriteLine(JObject.Parse(Encoding.UTF8.GetString(Base64Url.Decode(claims))));
+                }
+            }
+            else
+            {
+                if (response.ErrorType == TokenResponse.ResponseErrorType.Http)
+                {
+                    "HTTP error: ".ConsoleGreen();
+                    Console.WriteLine(response.Error);
+                    "HTTP status code: ".ConsoleGreen();
+                    Console.WriteLine(response.HttpStatusCode);
+                }
+                else
+                {
+                    "Protocol error response:".ConsoleGreen();
+                    Console.WriteLine(response.Json);
+                }
+            }
+        }
+
+        private static string CreateClientToken(string clientId, string audience)
+        {
+            var certificate = new X509Certificate2("client.pfx");
+            var now = DateTime.UtcNow;
+
+            var token = new JwtSecurityToken(
+                    clientId,
+                    audience,
+                    new List<Claim>()
+                    {
+                        new Claim("jti", Guid.NewGuid().ToString()),
+                        new Claim(JwtClaimTypes.Subject, clientId),
+                        new Claim(JwtClaimTypes.IssuedAt, now.ToEpochTime().ToString(), ClaimValueTypes.Integer64)
+                    },
+                    now,
+                    now.AddMinutes(1),
+                    new SigningCredentials(
+                        new X509SecurityKey(certificate),
+                        SecurityAlgorithms.RsaSha256
+                    )
+                );
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
