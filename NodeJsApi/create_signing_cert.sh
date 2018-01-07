733e733bcf3260f472c650caaff693745885bde7
Dominick Baier <dbaier@leastprivilege.com>
2018-01-07 12:03:23 +0100

    remove old samples

--- NodeJsApi/create_signing_cert.sh
+++
@@ -1,2 +0,0 @@
-openssl req -x509 -days 365 -newkey rsa:4096 -keyout key.pem -out cert.pem
-openssl pkcs12 -export -in cert.pem -inkey key.pem -out cert.pfx
--- NodeJsApi/start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-export HOST_IP=$(node $(pwd)/src/tools/ip.js)
-env | grep HOST_IP
-docker-compose kill && docker-compose up -d
